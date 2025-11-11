add46f261a2dc7a85b3bfec64adf68755ed44ddb
Denver John Demis <denverdemis@gmail.com>
2025-11-11 11:21:47 +0530

    Question 11

---
+++ Practice sheet 1/Question 11.c
@@ -0,0 +1,8 @@
+// Write a C program to find power of any number x ^ y.
+#include <stdio.h>
+int main(){
+    int x=2,y=5;
+    printf("%d raised to %d is %f",x,y,pow(x,y));
+     
+    return 0;
+}
