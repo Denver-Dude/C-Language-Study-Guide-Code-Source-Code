662dc7048e374a27e95933db68bf7ab0d09076cf
Denver John Demis <denverdemis@gmail.com>
2025-11-11 10:46:07 +0530

    Question 2

---
+++ Practice sheet 1/Question 2.c
@@ -0,0 +1,11 @@
+// Write a C program to perform input/output of all basic data types.
+#include <stdio.h>
+int main() {
+    int num1, num2, sum;
+    printf("please enter your first digit");
+    scanf("%d",&num1);
+    printf("please enter your second digit");
+    scanf("%d",&num2);
+    printf("The sum is %d",num1+num2);    
+    return 0;
+}
