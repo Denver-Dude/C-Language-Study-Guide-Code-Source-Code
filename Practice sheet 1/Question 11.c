adb5a9b87b67a3034a19a4b8d6e5b673bb05d7a8
Denver John Demis <denverdemis@gmail.com>
2025-11-11 11:25:17 +0530

    11 & 12

--- Practice sheet 1/Question 11.c
+++ Practice sheet 1/Question 11.c
@@ -1,7 +1,7 @@
 // Write a C program to find power of any number x ^ y.
 #include <stdio.h>
 int main(){
-    int x=2,y=5;
+    float x,y;
     printf("%d raised to %d is %f",x,y,pow(x,y));
      
     return 0;
---
+++ Practice sheet 1/Question 12.c
@@ -0,0 +1,8 @@
+// Write a C program to enter any number and calculate its square root.
+#include <stdio.h>
+int main(){
+    int value=2;
+    printf("%d 's square root is %f",value,pow(value,0.5));
+     
+    return 0;
+}
